9873bedfeaeb3a7ec0729812b024b983c5a5c6a5
T. Vega <tsvega7@gmail.com>
2020-05-03 21:29:15 -0400

    First commit

---
+++ src/shared/campsites.js
@@ -0,0 +1,39 @@
+export const CAMPSITES = [
+  {
+    id: 0,
+    name: "React Lake Campground",
+    image:"https://c0.wallpaperflare.com/preview/730/414/529/tent-mountain-flower-field.jpg",
+    
+    elevation: 1233,
+    featured: false,
+    description:
+      "Nestled in the foothills of the Chrome Mountains, this campground on the shores of the pristine React Lake is a favorite for fly fishers."
+  },
+  {
+    id: 1,
+    name: "Chrome River Campground ",
+    image: "https://media.mobilerving.com/mobilerving/uploads/37165/1.jpg",
+    elevation: 877,
+    featured: false,
+    description:
+      "Spend a few sunny days and starry nights beneath a canopy of old-growth firs at this enchanting spot by the Chrome River."
+  },
+  {
+    id: 2,
+    name: "Breadcrumb Trail Campground",
+    image: "/assets/images/breadcrumb-trail.jpg",
+    elevation: 2901,
+    featured: false,
+    description:
+      "Let NuCamp be your guide to this off-the-beaten-path, hike-in-only campground."
+  },
+  {
+    id: 3,
+    name: "Redux Woods Campground",
+    image: "https://content.active.com/Assets/Active.com+Content+Site+Digital+Assets/Kids/Galleries/10+Secrets/primitive-campsite.jpg",
+    elevation: 42,
+    featured: true,
+    description:
+      "You'll never want to leave this hidden gem, deep within the lush Redux Woods."
+  }
+];
